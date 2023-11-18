b0425f6aa315bb18fbf853a19b3c6c3b84ce28de
OUGO <hugo.parmentier@efrei.net>
2023-11-18 21:39:25 +0100

    4 pages : press enter loading Loading page Loging page Début de la page de nav

---
+++ main.py
@@ -0,0 +1,98 @@
+import pygame
+import sys
+from windows import LoadingWindow, TabbedInterface, StartPage, LoginPage
+
+# Initialisation de Pygame
+pygame.init()
+
+font_path = "Ressources/Sevastopol-Interface.ttf"  # Remplace avec le chemin de ta police
+
+# Définir les couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Créer la classe principale de l'application
+class Game:
+    def __init__(self):
+
+        info = pygame.display.Info()
+        self.SCREEN_WIDTH = info.current_w
+        self.SCREEN_HEIGHT = info.current_h
+
+        # Mettre en plein écran
+        self.screen = pygame.display.set_mode((self.SCREEN_WIDTH, self.SCREEN_HEIGHT), pygame.RESIZABLE)
+
+
+        # Initialiser les différentes fenêtres ou pages
+        self.start_page = StartPage(self.screen)
+        self.loading_window = LoadingWindow(self.screen)
+        self.login_page = LoginPage(self.screen)
+        self.tabbed_interface = TabbedInterface(self.screen)
+
+        # Définir l'état initial
+        self.current_window = self.start_page
+
+
+        pygame.display.set_caption("Interface Lamindanlsak")
+        self.clock = pygame.time.Clock()
+        self.is_running = True
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.is_running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.is_running = False  # Sortir du plein écran si la touche ESC est enfoncée
+
+    def switch_window(self, new_window):
+        # Changer la fenêtre courante
+        self.current_window = new_window
+
+    def handle_main_events(self, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.is_running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                self.is_running = False
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                if self.current_window == self.start_page:
+                    print ('ok')
+                    self.start_page.is_start_pressed = True
+
+    def run(self):
+        while self.is_running:
+            events = pygame.event.get()
+            self.handle_main_events(events)  # Nouvelle fonction pour traiter les événements principaux
+
+            if isinstance(self.current_window, StartPage) and self.current_window.is_start_pressed:
+                self.switch_window(self.loading_window)
+
+            if self.loading_window.is_loading_complete:
+                print("Loading complete")
+                self.switch_window(self.login_page)
+                self.loading_window.is_loading_complete = False
+
+            if self.login_page.is_login_complete:
+                print("Login complete")
+                self.switch_window(self.tabbed_interface)
+                self.login_page.is_login_complete = False
+
+            if self.current_window == self.login_page:
+                self.login_page.handle_events(events)
+
+            self.current_window.update()
+            self.current_window.draw()
+
+
+
+
+
+            pygame.display.flip()# Si tu veux utiliser une valeur différente de FPS, ajuste-la ici
+            self.clock.tick(60)
+
+# Code principal
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pygame.quit()
+    sys.exit()
---
+++ windows.py
@@ -0,0 +1,176 @@
+import pygame
+import sys
+
+
+# Initialisation de Pygame
+pygame.init()
+
+font_path = "Ressources/Sevastopol-Interface.ttf"
+text_color = (0, 255, 0)
+
+# Définir les couleurs
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+
+class StartPage:
+    def __init__(self, screen):
+        self.screen = screen
+        self.is_start_pressed = False
+        self.font = pygame.font.Font(font_path, 36)
+        self.start_text_base = "Appuyez sur Entrée pour démarrer"
+        self.start_text_rect = None
+        self.animation_frame = 0
+        self.animation_delay = 20
+        self.update_text()
+
+    def update_text(self):
+        dots = "." * (self.animation_frame // self.animation_delay % 4)
+        self.start_text = self.font.render(self.start_text_base + dots, True, text_color)
+        self.start_text_rect = self.start_text.get_rect(
+            bottomright=(self.screen.get_rect().bottomright[0] - 30,
+                         self.screen.get_rect().bottomright[1] - 30)
+        )
+
+    def update(self):
+        self.animation_frame += 1
+        self.update_text()
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        self.screen.blit(self.start_text, self.start_text_rect)
+        pygame.display.flip()
+
+# Créer la classe de la deuxième fenêtre (faux temps de chargement)
+class LoadingWindow:
+    def __init__(self, screen):
+        self.screen = screen
+        self.is_loading_complete = False
+        self.loading_bar_images = [pygame.image.load(f"Ressources/LoadingBars/LoadinBar{i:02d}.png") for i in range(16)]
+        self.loading_bar_index = 0
+
+    def update(self):
+        index = 0
+        while index < 10000000:
+            index += 1
+        if not self.is_loading_complete:
+            self.loading_bar_index += 1
+            if self.loading_bar_index >= len(self.loading_bar_images):
+                self.is_loading_complete = True
+
+    def draw(self):
+        self.screen.fill(BLACK)
+
+        # Dessiner la barre de chargement au centre de l'écran
+        if self.loading_bar_index < len(self.loading_bar_images):
+            loading_bar_image = self.loading_bar_images[self.loading_bar_index]
+            bar_rect = loading_bar_image.get_rect(center=self.screen.get_rect().center)
+            self.screen.blit(loading_bar_image, bar_rect)
+
+        pygame.display.flip()
+
+class LoginPage:
+    def __init__(self, screen):
+        self.screen = screen
+
+        # Couleurs
+        self.is_login_complete = False
+        self.is_login_failed = False
+        self.text_color = (0, 0, 0)
+        self.input_box_color = (200, 200, 200)
+
+        # Police
+        self.font = pygame.font.Font(font_path, 36)
+
+        # Entrées
+        self.password = ""
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.QUIT:
+                self.is_running = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    if self.password == "password":
+                        self.is_login_complete = True
+                        print("Login successful")
+                    else:
+                        self.is_login_failed = True
+                        print("Login failed")
+                elif event.key == pygame.K_BACKSPACE:
+                    # Gestion de la suppression
+                    self.password = self.password[:-1]
+                elif event.unicode:  # Vérifier si le caractère unicode est disponible
+                    char = event.unicode
+                    self.password += char
+                    print(self.password)
+
+
+    def update(self):
+        if self.is_login_complete:
+            # Vous pouvez ajouter ici la logique pour traiter les informations de connexion
+            print("Password entered:", self.password)
+
+    def draw(self):
+        self.screen.fill(BLACK)
+
+        welcome_message = "Bienvenue Professeur Lamindanlésak"
+        welcome_font = pygame.font.Font(font_path, 50)  # Police ajustée
+        welcome_text_surface = welcome_font.render(welcome_message, True, text_color)
+        welcome_text_rect = welcome_text_surface.get_rect(center=(self.screen.get_width() // 2, 30))
+        self.screen.blit(welcome_text_surface, welcome_text_rect)
+
+
+        # Calcul de la position centrée de l'entrée du mot de passe
+        input_box_x = (self.screen.get_width() - 240) // 2
+        input_box_y = int(self.screen.get_height() * 0.55)
+
+        # Dimensions ajustées
+        input_box_width = 280
+        input_box_height = 50
+
+        # Couleurs
+        border_color = (0, 255, 0)  # Vert pour les bordures
+        inside_color = BLACK
+
+        # Affichage du champ de mot de passe
+        pygame.draw.rect(self.screen, border_color, (input_box_x, input_box_y, input_box_width, input_box_height))
+        pygame.draw.rect(self.screen, inside_color, (
+        input_box_x + 2, input_box_y + 2, input_box_width - 4, input_box_height - 4))  # Intérieur en noir
+
+        #pygame.draw.rect(self.screen, text_color, (input_box_x + input_box_width, input_box_y, 2, 30))
+        if self.is_login_failed:
+            text_surface = self.font.render("Mot de passe incorrect", True, (255, 0, 0))
+            self.screen.blit(text_surface, (input_box_x - 170, input_box_y + 60))
+
+        # Affichage du texte "Mot de Passe :" à gauche de l'entrée
+        text_surface = self.font.render("Mot de Passe :", True, text_color)
+        self.screen.blit(text_surface, (input_box_x - 170, input_box_y+8))
+
+        # Affichage du mot de passe masqué
+        password_text_surface = self.font.render(self.password, True, text_color)
+        self.screen.blit(password_text_surface, (input_box_x + 10, input_box_y + 10))
+
+        pygame.display.flip()
+
+
+class TabbedInterface:
+    def __init__(self, screen):
+        self.screen = screen
+        self.current_tab = 0  # Indice de l'onglet actuel
+
+    def handle_events(self):
+        # Gérer les événements liés aux onglets
+        return
+        # ...
+
+    def update(self):
+        # Logique de mise à jour de l'interface avec onglets
+        # ...
+        return
+
+    def draw(self):
+        self.screen.fill(BLACK)
+        # Dessiner les éléments de l'interface avec onglets
+        pygame.display.flip()
+
